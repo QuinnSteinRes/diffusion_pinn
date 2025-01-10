847f0c9eefab828c070e1d44202c653b3eca5408
Quinn Stein <qs8@hw.ac.uk>
2025-01-10 15:53:47 +0000

    Plotting consolidated

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -3,3 +3,9 @@ from .visualization import (
     plot_diffusion_convergence,
     plot_loss_history
 )
+
+__all__ = [
+    'plot_solutions_and_error',
+    'plot_diffusion_convergence',
+    'plot_loss_history'
+]
