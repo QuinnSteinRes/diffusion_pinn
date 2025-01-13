75300740a70b6e00270d1c0536e4a6754aed7788
Quinn Stein <qs8@hw.ac.uk>
2025-01-13 17:07:10 +0000

    Update __init__ for relative imports

--- src/diffusion_pinn/__init__.py
+++ src/diffusion_pinn/__init__.py
@@ -1,6 +1,6 @@
-from diffusion_pinn.config import DiffusionConfig
-from diffusion_pinn.models.pinn import DiffusionPINN
-from diffusion_pinn.data.processor import DiffusionDataProcessor
-from diffusion_pinn.utils.visualization import plot_loss_history, plot_diffusion_convergence  # Fixed function name
+from .config import DiffusionConfig
+from .models.pinn import DiffusionPINN
+from .data.processor import DiffusionDataProcessor
+from .utils.visualization import plot_loss_history, plot_diffusion_convergence  # Fixed function name
 
 __version__ = '0.1.0'
--- ttt.py
+++
@@ -1,4 +0,0 @@
-import diffusion_pinn
-from diffusion_pinn import models
-print("Package path:", diffusion_pinn.__file__)
-print("Available modules:", dir(diffusion_pinn))
