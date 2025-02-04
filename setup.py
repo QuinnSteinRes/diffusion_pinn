12494de7533a020d15247b7f11bbe325e6fc01bd
Quinn Stein <qs8@hw.ac.uk>
2025-02-04 12:08:49 +0000

    Version number update and version location changed to a single location in src/diffusion_pinn/__init__.py

--- setup.py
+++ setup.py
@@ -8,9 +8,12 @@ with open('requirements.txt') as f:
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
 
+# In setup.py
+from diffusion_pinn import __version__
+
 setup(
     name="diffusion_pinn",
-    version="0.1.0",
+    version=__version__,
     description="Physics-Informed Neural Network for Diffusion Problems",
     long_description=long_description,
     long_description_content_type="text/markdown",
--- src/diffusion_pinn/__init__.py
+++ src/diffusion_pinn/__init__.py
@@ -3,4 +3,4 @@ from .models.pinn import DiffusionPINN
 from .data.processor import DiffusionDataProcessor
 from .utils.visualization import plot_loss_history, plot_diffusion_convergence  # Fixed function name
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
