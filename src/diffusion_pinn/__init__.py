74e7597859ad5585b37f99cfe934927c30eda6b2
Quinn Stein <qs8@hw.ac.uk>
2025-02-06 15:23:51 +0000

    Update to __init__.py to include variables

--- src/diffusion_pinn/__init__.py
+++ src/diffusion_pinn/__init__.py
@@ -2,5 +2,6 @@ from .config import DiffusionConfig
 from .models.pinn import DiffusionPINN
 from .data.processor import DiffusionDataProcessor
 from .utils.visualization import plot_loss_history, plot_diffusion_convergence  # Fixed function name
+from .variables import PINN_VARIABLES
 
-__version__ = '0.1.1'
+__version__ = '0.2.0'
