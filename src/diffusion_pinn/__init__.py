843a631ed932e7d82bfe3d8d01064fa880a4d0ff
Quinn Stein <qs8@hw.ac.uk>
2025-02-07 15:34:05 +0000

    0.2.4: Initial D changed to 0.001 for smallD testing

--- src/diffusion_pinn/__init__.py
+++ src/diffusion_pinn/__init__.py
@@ -4,4 +4,4 @@ from .data.processor import DiffusionDataProcessor
 from .utils.visualization import plot_loss_history, plot_diffusion_convergence  # Fixed function name
 from .variables import PINN_VARIABLES
 
-__version__ = '0.2.3'
+__version__ = '0.2.4'
