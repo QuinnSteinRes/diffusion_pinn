9c3077d4eaadfde1d16b451bc908771ac6691ac7
Quinn Stein <qs8@hw.ac.uk>
2025-04-10 10:50:25 +0100

    0.2.7-1 Update to variables enmass to trial narrower convergance

--- src/diffusion_pinn/variables.py
+++ src/diffusion_pinn/variables.py
@@ -4,18 +4,18 @@ Configuration variables for PINN training
 
 PINN_VARIABLES = {
     # Network Architecture
-    'hidden_layers': [40, 40, 40],
+    'hidden_layers': [60, 60, 60], # 0.2.7 [40, 40, 40]
     'activation': 'tanh',
 
     # Training Parameters
     'epochs': 50000,
-    'learning_rate': 0.001,
-    'decay_steps': 1000,
-    'decay_rate': 0.95,
-    'initial_D': 0.001,
+    'learning_rate': 0.0005, # 0.2.7 0.001
+    'decay_steps': 2000, # 0.2.7 1000
+    'decay_rate': 0.90,  # 0.2.7 0.95
+    'initial_D': 0.0005, # 0.2.7 0.001
 
     # Sampling Points
-    'N_u': 1000,    # boundary points
+    'N_u': 2000,    # boundary points       0.2.7 1000
     'N_f': 20000,   # collocation points
     'N_i': 10000,   # interior points
 
@@ -24,6 +24,6 @@ PINN_VARIABLES = {
         'initial': 1.0,
         'boundary': 1.0,
         'interior': 10.0,
-        'physics': 5.0
+        'physics': 10.0    #0.2.7 5.0
     }
 }
