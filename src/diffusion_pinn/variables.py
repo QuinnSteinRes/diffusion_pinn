a7d10231c8e0dd9becb5905c5dfc200807488d82
Quinn Stein <qs8@hw.ac.uk>
2025-04-11 16:29:42 +0100

    0.2.10 Aditonal layers added

--- src/diffusion_pinn/variables.py
+++ src/diffusion_pinn/variables.py
@@ -4,11 +4,11 @@ Configuration variables for PINN training
 
 PINN_VARIABLES = {
     # Network Architecture
-    'hidden_layers': [60, 60, 60], # 0.2.7 [40, 40, 40]
+    'hidden_layers': [40, 40, 40, 40, 40], # 0.2.7 [40, 40, 40]
     'activation': 'tanh',
 
     # Training Parameters
-    'epochs': 50000,
+    'epochs': 50001,
     'learning_rate': 0.0005, # 0.2.7 0.001
     'decay_steps': 2000, # 0.2.7 1000
     'decay_rate': 0.90,  # 0.2.7 0.95
