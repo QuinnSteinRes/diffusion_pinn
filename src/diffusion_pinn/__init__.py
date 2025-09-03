d5d911efe4a914d59b7173476862e690837a669e
Quinn Stein <qs8@hw.ac.uk>
2025-09-03 19:07:25 +0100

    0.2.28.7 Initial 5

--- src/diffusion_pinn/__init__.py
+++ src/diffusion_pinn/__init__.py
@@ -17,7 +17,7 @@ except ImportError:
     OPTIMIZATION_SETTINGS = {}
     _has_optimization = False
 
-__version__ = '0.2.28.6'
+__version__ = '0.2.28.7'
 
 # Base components that should always be available
 __all__ = [
