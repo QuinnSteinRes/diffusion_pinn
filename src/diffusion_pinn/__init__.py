0402855c258b851be9572faa2133bc73e4fd71ba
Quinn Stein <qs8@hw.ac.uk>
2025-09-04 08:38:19 +0100

    0.2.28.8 Initial 10

--- src/diffusion_pinn/__init__.py
+++ src/diffusion_pinn/__init__.py
@@ -17,7 +17,7 @@ except ImportError:
     OPTIMIZATION_SETTINGS = {}
     _has_optimization = False
 
-__version__ = '0.2.28.7'
+__version__ = '0.2.28.8'
 
 # Base components that should always be available
 __all__ = [
--- src/diffusion_pinn/variables.py
+++ src/diffusion_pinn/variables.py
@@ -20,7 +20,7 @@ PINN_VARIABLES = {
 
     # Loss Weights - Using v0.2.14 proven weighting that ensures good concentration fields
     'loss_weights': {
-        'initial': 5.0,
+        'initial': 10.0,
         'boundary': 1.0,
         'interior': 1.0,   # Strong emphasis on data fitting
         'physics': 1.0      # Strong physics enforcement
